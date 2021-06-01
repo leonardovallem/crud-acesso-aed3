9301efe65e377c8d24df5745f7af0463db39319b
Leonardo Mourão <leonardo.mourao@hotmart.com>
2021-06-01 18:32:22 -0300

    correção do arquivo de constantes

--- src/config/Const.java
+++ src/config/Const.java
@@ -3,6 +3,7 @@ package config;
 public class Const {
     public static String FilesPath = "src/files/";
     public static String PerguntasDB = "perguntas.db";
-    public static String RespostasDB = "respostas.db";
+    public static String RespostasOfPerguntaDB = "pergunta.respostas.db";
+    public static String RespostasOfUsuarioDB = "usuario.respostas.db";
     public static String UsuariosDB = "usuarios.db";
 }
