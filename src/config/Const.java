e055006809eaa7a679aa0d2d0fc77b026e18e260
vitorcrp <66389015+vitorcrp@users.noreply.github.com>
2021-06-01 17:51:11 -0300

    send Const to config

---
+++ src/config/Const.java
@@ -0,0 +1,8 @@
+package config;
+
+public class Const {
+    public static String FilesPath = "src/files/";
+    public static String PerguntasDB = "perguntas.db";
+    public static String RespostasDB = "respostas.db";
+    public static String UsuariosDB = "usuarios.db";
+}
